e3dbf0aa20f6c34ba5e944625afe9005e2a2cac6
Anjil Dhamala <adhamala@ensocare.com>
2018-07-13 15:40:09 -0500

    added postinstall script to create type definition files for better tsc support.

--- index.ts
+++
@@ -1,5 +0,0 @@
-export * from './src/touch.module';
-export * from './src/touch.driver';
-export * from './src/devices/ios/ios.touch';
-export * from './src/devices/android/android.touch';
-export * from './src/storage/touch.storage';
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './touch.module';
+export * from './touch.driver';
+export * from './constants/touch.constants';
+export * from './interfaces/credentials.interface';
