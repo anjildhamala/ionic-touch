89311bfd3d251a5a3ee6df9f4c74bf1cd881f9b4
Anjil Dhamala <adhamala@ensocare.com>
2018-09-21 23:31:18 -0500

    updated touch storage to throw error when android key is not found

--- src/storage/touch.storage.ts
+++ src/storage/touch.storage.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { SecureStorage, SecureStorageObject } from "@ionic-native/secure-storage";
-import { TOUCH_CONSTANTS } from "../constants/touch.constants";
+import { TOUCH_ANDROID_ERRORS, TOUCH_CONSTANTS } from "../constants/touch.constants";
 import { WordArray } from "crypto-js";
 
 @Injectable()
@@ -14,23 +14,26 @@ export class TouchSecureStorage {
 
   saveCredentials(encryptedCredentials: WordArray) {
     return this.init()
-      .then((storageInstance: SecureStorageObject) => storageInstance.set(TOUCH_CONSTANTS.storedKey, encryptedCredentials.toString()));
+        .then((storageInstance: SecureStorageObject) => storageInstance.set(TOUCH_CONSTANTS.storedKey, encryptedCredentials.toString()));
   }
 
   saveAndroidKey(key: WordArray) {
     return this.init()
-      .then((storageInstance: SecureStorageObject) => storageInstance.set(TOUCH_CONSTANTS.androidTokenKey, key.toString()));
+        .then((storageInstance: SecureStorageObject) => storageInstance.set(TOUCH_CONSTANTS.androidTokenKey, key.toString()));
   }
 
   getCredentials(): Promise<WordArray> {
     return this.init()
-      .then((storageInstance: SecureStorageObject) => storageInstance.get(TOUCH_CONSTANTS.storedKey))
-      .then((encryptedCredentials: any) => encryptedCredentials);
+        .then((storageInstance: SecureStorageObject) => storageInstance.get(TOUCH_CONSTANTS.storedKey))
+        .then((encryptedCredentials: any) => encryptedCredentials);
   }
 
   getAndroidKey(): Promise<WordArray> {
     return this.init()
-      .then((storageInstance: SecureStorageObject) => storageInstance.get(TOUCH_CONSTANTS.androidTokenKey))
-      .then((encryptedToken: any) => encryptedToken);
+        .then((storageInstance: SecureStorageObject) => storageInstance.get(TOUCH_CONSTANTS.androidTokenKey))
+        .then((encryptedToken: any) => encryptedToken)
+        .catch(() => {
+          throw TOUCH_ANDROID_ERRORS.ANDROID_KEY_NOT_FOUND.key;
+        })
   }
 }
